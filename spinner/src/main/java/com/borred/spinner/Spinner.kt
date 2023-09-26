561ecf2f18e9cc26db5e5e9e9272a3a7742d96de
yerlan sarsenov <erlan@1fit.app>
2023-09-26 22:54:21 +0600

    develop: add one item case in Spinner

--- spinner/src/main/java/com/borred/spinner/Spinner.kt
+++ spinner/src/main/java/com/borred/spinner/Spinner.kt
@@ -1,27 +1,64 @@
 package com.borred.spinner
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Spinner(
     count: Int,
-    size: Dp,
+    itemSize: Dp,
     modifier: Modifier = Modifier
 ) {
     Layout(
         modifier = modifier,
         content = {
             repeat(count) {
-                SpinnerItem(modifier = Modifier.size(size))
+                SpinnerItem(
+                    index = it,
+                    modifier = Modifier.size(itemSize)
+                )
             }
         }
     ) { measurables, constraints ->
-        layout(width = constraints.maxWidth, height = constraints.maxHeight) {
-
+        val minSide = minOf(constraints.maxWidth, constraints.maxHeight)
+        layout(width = minSide, height = minSide) {
+            if (measurables.size == 1) {
+                val halfItemSizePx = (itemSize / 2).roundToPx()
+                measurables.single().measure(
+                    constraints.copy(
+                        minWidth = 0,
+                        maxWidth = minSide,
+                        minHeight = 0,
+                        maxHeight = minSide
+                    )
+                ).place(
+                    x = minSide / 2 - halfItemSizePx,
+                    y = minSide / 2 - halfItemSizePx
+                )
+                return@layout
+            }
         }
     }
+}
+
+@Preview
+@Composable
+private fun Spinner_Preview(
+    modifier: Modifier = Modifier
+) {
+    Spinner(
+        count = 1,
+        itemSize = 50.dp,
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
+    )
 }
--- spinner/src/main/java/com/borred/spinner/SpinnerItem.kt
+++ spinner/src/main/java/com/borred/spinner/SpinnerItem.kt
@@ -3,16 +3,21 @@ package com.borred.spinner
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlin.random.Random
 
 @Composable
 internal fun SpinnerItem(
+    index: Int,
     modifier: Modifier = Modifier
 ) {
     val color = rememberRandomColor()
@@ -22,7 +27,17 @@ internal fun SpinnerItem(
             .drawBehind {
                 drawCircle(color = color)
             }
-    )
+    ) {
+        Text(
+            text = "$index",
+            color = if (color == Color.Black) {
+                Color.White
+            } else {
+                Color.Black
+            },
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
 
 @Composable
@@ -34,4 +49,13 @@ private fun rememberRandomColor(): Color {
             blue = Random.nextInt(from = 0, until = 0x100)
         )
     }
+}
+
+@Preview
+@Composable
+private fun SpinnerItem_Preview() {
+    SpinnerItem(
+        index = 1,
+        modifier = Modifier.size(50.dp)
+    )
 }
